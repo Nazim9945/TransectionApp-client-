d79e60d76664e2af527da087b4a94be7d80cfbaf
Nazim Saifi <saifinazim2311@gmail.com>
2024-09-04 01:13:08 +0530

    all services working fine

--- src/pages/Transfer.tsx
+++ src/pages/Transfer.tsx
@@ -1,10 +1,58 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import Inputfield from "../components/Inputfield";
+import { useState } from "react";
+import Button from "../components/Button";
+import axios from "axios";
+import { commonUrl } from "../config/commonUrl";
 
 const Transfer = () => {
+     const [searchParams] = useSearchParams()
+     const navigate=useNavigate()
+     const[amount,setAmount]=useState("")
+     const username=searchParams.get("firstName")
+       const id = searchParams.get("id");
   return (
-    <div>
-        hello from transfer
+    <div className="border-2 border-slate-300">
+      <div>Send Money</div>
+      <div>
+        <div>
+          {username}
+          <span className="rounded-full bg-black text-white p-2 ml-2">
+            {username?.[0].toUpperCase()}
+          </span>
+        </div>
+      </div>
+      <Inputfield
+        onhandler={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setAmount(e.target.value)
+        }
+        type="text"
+        placeholder="Enter Amt..."
+      />
+      <Button
+        onsubmit={() =>
+          axios
+            .post(
+              `${commonUrl}/account/transfer`,
+              {
+                to: id,
+                amount: amount,
+              },
+              {
+                headers: {
+                  token: localStorage.getItem("token"),
+                },
+              }
+            )
+            .then((res) =>(
+                console.log(res.data),
+                navigate('/dashboard')
+            ) )
+        }
+        label="Intiate Transfer"
+      />
     </div>
-  )
+  );
 }
 
 export default Transfer
